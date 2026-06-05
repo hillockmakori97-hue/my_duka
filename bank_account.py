d46869b32ec07d0e241c83261b7ddca3e10a12e5
hillockmakori97-hue <hillockmakori97@gmail.com>
2026-06-05 16:19:48 +0300

    bank account

---
+++ bank_account.py
@@ -0,0 +1,31 @@
+class Bank_account:
+    def __init__(self,account_no,balance,owner_name,date_opened):
+        self.balance=balance
+        self.account_no=account_no
+        self.owner_name=owner_name
+        self.date_opened=date_opened
+
+    def check_balance(self,balance):
+        print(f'you have a balance of Ksh {self.balance}')
+    def withdraw(self,amount):
+        if self.balance>amount:
+            self.balance=self.balance-amount
+            res='withdrawal successful'
+        else:
+            res='balance too low, deposit and try again'
+        return res
+    def check_details(self):
+        print(f''' 
+        user_details 
+        owner_name={self.owner_name}
+        account_number={self.account_no}
+        Account opened on {self.date_opened}
+        ''')
+    def close_account(self,account):
+        del account
+        print(f'account {account} was successfuly deleted')
+acc1=Bank_account('ac1',40000,'john','12/4/2021')
+acc1.withdraw(14000)
+print(acc1.balance) 
+acc1.close_account(acc1)
+
---
+++ objects.py
@@ -0,0 +1,18 @@
+class Student:
+    def __init__(self,name,student_no,course):
+        self.name=name 
+        self.student_no=student_no
+        self.course=course
+    
+    def study (self):
+        print(f'{self.name} studies')
+    def details (self):
+        print(f'{self.name} student number{self.student_no} studies {self.course}')
+
+#object 1
+student1=Student('jack','st1','Computer_science')
+print(student1.name)
+
+#object 2 
+student2=Student('jane','st2','computer_engineering')
+student2.details()
